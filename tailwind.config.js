f1993c498875b887f26673d993a7de79a50df617
Fernando Santos <fern@ndo.io>
2019-07-20 22:27:35 -0300

    Adicionando e Configurando o Tailwind CSS 🙈

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    theme: {
+        colors: {
+            transparent: "transparent",
+
+            black: "#000",
+            white: "#fff",
+            error: "#c53030",
+            warning: "#b7791f",
+            success: "#2f855a",
+            primary: "#6b46c1",
+            secondary: "#4a5568",
+            codigofalado: "#EB007D",
+
+            gray: {
+                100: "#f7fafc",
+                200: "#edf2f7",
+                300: "#e2e8f0",
+                400: "#cbd5e0",
+                500: "#a0aec0",
+                600: "#718096",
+                700: "#4a5568",
+                800: "#2d3748",
+                900: "#1a202c"
+            }
+        },
+        extend: {}
+    },
+    variants: {},
+    plugins: []
+};
