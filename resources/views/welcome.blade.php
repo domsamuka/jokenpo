b3d855dd82fd6b41d6afab2469839d8004477c8f
Fernando Santos <fern@ndo.io>
2019-10-27 02:19:44 -0300

    Removing Tests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,6 @@
 
 @section('content')
     <div class="title m-b-md">
-        JoKenPô, <strong>Bicho</strong>!
-        <h1>WEEEE ARE THE CHAMPIONS!</h1>
-        <h2>It Works!</h2>
+        JoKenPô!
     </div>
 @endsection
