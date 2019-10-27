a0c47a1830bdf3e48f7a05052e42e16b57e55e3e
Fernando Santos <fern@ndo.io>
2019-10-27 02:17:53 -0300

    Final Deployment Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
 @section('content')
     <div class="title m-b-md">
         JoKenPô, <strong>Bicho</strong>!
-        <h1>Eita, que não funciona nunca!</h1>
+        <h1>WEEEE ARE THE CHAMPIONS!</h1>
+        <h2>It Works!</h2>
     </div>
 @endsection
