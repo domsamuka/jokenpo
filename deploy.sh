20df1fde55a57bf6578e043aab5fe3045c728fc0
Fernando Santos <fern@ndo.io>
2019-10-27 02:15:44 -0300

    SUDO removido do deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # activate maintenance mode
-sudo /opt/bitnami/php/bin/php artisan down
+/opt/bitnami/php/bin/php artisan down
 # update source code
-sudo /usr/bin/git pull
+/usr/bin/git pull
 # update PHP dependencies
-sudo /opt/bitnami/php/bin/composer install --no-interaction --no-dev --prefer-dist
+/opt/bitnami/php/bin/composer install --no-interaction --no-dev --prefer-dist
 # --no-interaction Do not ask any interactive question
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
 # update database
-sudo /opt/bitnami/php/bin/php artisan migrate --force
+/opt/bitnami/php/bin/php artisan migrate --force
 # --force  Required to run when in production.
 # stop maintenance mode
-sudo /opt/bitnami/php/bin/php artisan up
+/opt/bitnami/php/bin/php artisan up
