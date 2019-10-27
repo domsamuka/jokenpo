92fc943f763b2896490e55b5cc2168270f6b7902
Fernando Santos <fern@ndo.io>
2019-10-27 02:03:55 -0300

    Adicionado path absoluto no script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # activate maintenance mode
-sudo php artisan down
+sudo /opt/bitnami/php/bin/php artisan down
 # update source code
-sudo git pull
+sudo /usr/bin/git pull
 # update PHP dependencies
-sudo composer install --no-interaction --no-dev --prefer-dist
+sudo /opt/bitnami/php/bin/composer install --no-interaction --no-dev --prefer-dist
 # --no-interaction Do not ask any interactive question
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
 # update database
-sudo php artisan migrate --force
+sudo /opt/bitnami/php/bin/php artisan migrate --force
 # --force  Required to run when in production.
 # stop maintenance mode
-sudo php artisan up
+sudo /opt/bitnami/php/bin/php artisan up
